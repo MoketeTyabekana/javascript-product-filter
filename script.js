5fb0e91e06799eff3fdccd0240b90f8fff744290
Mokete <emtyabekana@gmail.com>
2024-10-21 12:42:20 +0200

    Added a scroll overflow on our products container.

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const search = () => {
   const searchBox = document.getElementById("search-item").value.toUpperCase();
   const storeItems = document.getElementById("products-list");
   const product = storeItems.querySelectorAll("product");
-  const productName = document.getElementsByTagName(h4);
+  const productName = document.getElementsByTagName("h4");
 
-  for (let i = 0; i < productName; i++) {
-    let match = product[i].getElementsByTagName("h4")[0];
+  for (const i = 0; i < productName; i++) {
+    const match = product[i].getElementsByTagName("h4")[0];
 
     if (match) {
-      let textvalue = match.textContent || match.innerHTML;
+      const textvalue = match.textContent || match.innerHTML
 
       if (textvalue.toUpperCase().indexOf(searchBox) > -1) {
         product[i].style.display = "";

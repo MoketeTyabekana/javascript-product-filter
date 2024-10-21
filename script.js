fe116b901111ba81ddb93ec83078cbe765098892
Mokete <emtyabekana@gmail.com>
2024-10-21 12:26:07 +0200

    Used the if statement to validate the our search, first getting the our product name through the h4 tag, and assigning it's value to match. Then used an if statement to check if the product name matches the textContent of any product and display the matching products.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const search =()=>{
-    const searchBox=document.getElementById('search-item').value.toUpperCase();
-}
+const search = () => {
+  const searchBox = document.getElementById("search-item").value.toUpperCase();
+  const storeItems = document.getElementById("products-list");
+  const product = storeItems.querySelectorAll("product");
+  const productName = document.getElementsByTagName(h4);
+
+  for (let i = 0; i < productName; i++) {
+    let match = product[i].getElementsByTagName("h4")[0];
+
+    if (match) {
+      let textvalue = match.textContent || match.innerHTML;
+
+      if (textvalue.toUpperCase().indexOf(searchBox) > -1) {
+        product[i].style.display = "";
+      }
+    } else {
+      product[i].style.display = "none";
+    }
+  }
+};

f472643668093a91d3736ac652e7ed878f697e6a
Mokete <emtyabekana@gmail.com>
2024-10-21 13:01:18 +0200

    Placed the else statement correctly after its if statement.

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const search = () => {
   const searchBox = document.getElementById("search-item").value.toUpperCase();
   const storeItems = document.getElementById("products-list");
-  const product = storeItems.querySelectorAll("product");
+  const product = storeItems.querySelectorAll(".product");
   const productName = document.getElementsByTagName("h4");
 
-  for (const i = 0; i < productName; i++) {
-    const match = product[i].getElementsByTagName("h4")[0];
+  for (var i = 0; i < productName.length; i++) {
+    let match = product[i].getElementsByTagName("h4")[0];
 
     if (match) {
-      const textvalue = match.textContent || match.innerHTML
+      let textvalue = match.textContent || match.innerHTML;
 
       if (textvalue.toUpperCase().indexOf(searchBox) > -1) {
         product[i].style.display = "";
+      }else {
+        product[i].style.display = "none";
       }
-    } else {
-      product[i].style.display = "none";
-    }
+    } 
   }
 };
